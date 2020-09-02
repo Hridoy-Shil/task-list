3dff111d6e99a7ac42aad845e6434ded85f47998
Hridoy <65807939+Hridoy-Shil@users.noreply.github.com>
2020-09-03 01:20:31 +0600

    javaScript Second Project

--- script.js
+++ script.js
@@ -1,126 +1,126 @@
-//Define ul  Element
-let form = document.querySelector("#task_form");
-let taskInput = document.querySelector("#new_task");
-let filter = document.querySelector("#task_filter");
-let taskList = document.querySelector("ul");
-let clearBtn = document.querySelector("#clear_task_btn");
+//get ui element
+let form = document.querySelector('#book-form');
+let bookList = document.querySelector('#book-list');
 
-//Define Eventlistener
 
-form.addEventListener("submit", addTask);
-taskList.addEventListener("click", removeTask);
-clearBtn.addEventListener("click", clearTask);
-filter.addEventListener("keyup", filterTask);
-document.addEventListener("DOMContentLoaded", getTask);
+//Book class
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+
+class UI {
+  static addToBookList(book) {
+    let list = document.querySelector('#book-list');
+    let row = document.createElement('tr');
+    row.innerHTML = `
+   <td>${book.title}</td>
+   <td>${book.author}</td>
+   <td>${book.isbn}</td>
+   <td><a href ='#'class="delete"> X</a> </td>
+   `
+    list.appendChild(row);
+  }
+  static clearFields() {
+    document.querySelector('#title').value = '',
+      document.querySelector('#author').value = '',
+      document.querySelector('#isbn').value = '';
+  }
+  static showAlert(message, className) {
+    let div = document.createElement('div');
+    div.className = `alert ${className}`;
+    div.appendChild(document.createTextNode(message));
+    let container = document.querySelector('.container');
+    let form = document.querySelector('#book-form');
+    container.insertBefore(div, form);
+
+
+    setTimeout(() => {
+      document.querySelector('.alert').remove()
+    }, 2000);
+  }
+  static deleteFormBook(target) {
+    if (target.hasAttribute('href')) {
+      (target.parentElement.parentElement.remove());
+
+      Store.removeBook(target.parentElement.previousElementSibling.textContent.trim());
+
+      UI.showAlert('Remove Book !', 'success');
+    }
+  }
+}
+
+
+//Local storage class 
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+    return books;
+  }
+  static addBook(book) {
+    let books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static displayBooks() {
+    let books = Store.getBooks();
+
+    books.forEach(book => {
+      UI.addToBookList(book);
+    })
+  }
+  static removeBook(isbn) {
+    let books = Store.getBooks();
+    books.forEach((book, index) => {
+      if (book.isbn === isbn) {
+        books.splice(index, 1);
+
+      }
+    })
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+//addEventListener
+form.addEventListener('submit', newBook);
+bookList.addEventListener('click', removeBook);
+document.addEventListener('DOMEcontentLoaded', Store.displayBooks());
 
 //Define function
-function addTask(e) {
-  if (taskInput.value === "") {
-    alert("Add a task");
+function newBook(e) {
+
+  let title = document.querySelector('#title').value,
+    author = document.querySelector('#author').value,
+    isbn = document.querySelector('#isbn').value;
+
+  let book = new Book(title, author, isbn);
+
+  if (title === '' || author === '' || isbn === '') {
+    UI.showAlert('Please fill all the filds !', 'error')
   } else {
-    //creat li
-    let li = document.createElement("li");
-    li.appendChild(document.createTextNode(taskInput.value + " "));
-    taskList.appendChild(li);
-    let link = document.createElement("a");
-    link.setAttribute("href", "#");
-    link.innerHTML = "X";
-    li.appendChild(link);
+    UI.addToBookList(book);
+    UI.clearFields();
+    UI.showAlert('Book Added !', 'success');
 
-    //local store
 
-    storeTaskInLocalStorage(taskInput.value);
-    taskInput.value = " ";
+    Store.addBook(book);
   }
+
   e.preventDefault();
 }
 
-//removeTask
-function removeTask(e) {
-  if (e.target.hasAttribute("href")) {
-    if (confirm("are you sur ?")) {
-      let ele = e.target.parentElement;
-      ele.remove();
-      //console.log(e.target);
+function removeBook(e) {
+  UI.deleteFormBook(e.target);
 
-      removeFormLS(ele);
-    }
-  }
-}
 
-//clearTask
-
-function clearTask(e) {
-  //taskList.innerHTML = '';
-
-  while (taskList.firstChild) {
-    taskList.removeChild(taskList.firstChild);
-  }
-  localStorage.clear();
-}
-
-//filterTask
-
-function filterTask(e) {
-  let text = e.target.value.toLowerCase();
-  document.querySelectorAll("li").forEach((task) => {
-    let item = task.firstChild.textContent;
-
-    if (item.toLowerCase().indexOf(text) != -1) {
-      task.style.display = "block";
-    } else {
-      task.style.display = "none";
-    }
-  });
-}
-
-// store in local store
-
-function storeTaskInLocalStorage(task) {
-  let tasks;
-  if (localStorage.getItem("tasks") === null) {
-    tasks = [];
-  } else {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-  }
-  tasks.push(task);
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-}
-
-function getTask() {
-  let tasks;
-  if (localStorage.getItem("tasks") === null) {
-    tasks = [];
-  } else {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-  }
-  tasks.forEach((task) => {
-    let li = document.createElement("li");
-    li.appendChild(document.createTextNode(task + " "));
-    taskList.appendChild(li);
-    let link = document.createElement("a");
-    link.setAttribute("href", "#");
-    link.innerHTML = "X";
-    li.appendChild(link);
-  });
-}
-
-//remove task form ls
-
-function removeFormLS(taskItem) {
-  let tasks;
-  if (localStorage.getItem("tasks") === null) {
-    tasks = [];
-  } else {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-  }
-
-  let li = taskItem;
-  li.removeChild(li.lastChild); //<a></a>
-  tasks.forEach((task, index) => {
-    if (li.textContent.trim() === task) {
-      tasks.splice(index, 1);
-    }
-  });
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+  e.preventDefault();
 }
